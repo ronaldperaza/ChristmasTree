0ef1cfa64b4233c762e898b67a67de43efd90b17
Ronald Peraza <ronaldperaza460@gmail.com>
2020-12-24 21:22:55 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,30 +2,28 @@ from time import sleep
 from random import randint
 from colorama import Fore, Style
 
-# COLORES DISPONIBLES PARA Fore
-# BLACK, RED GREEN YELLOW, BLUE,
-# MAGENTA, CYAN WHITHE
+message_to_show = input("Enter a message to show: ")
 
-texto1=input("Ingresa un Mensaje de Navidad: ")
-#texto1=str(texto1)
-base1=len(texto1)
-base1+=1
+# if text contains a number convert to string
+message_to_show = str(message_to_show)
+
+base = len(message_to_show)
+base += 1
 
 while True:
-    print('\033c')  #para limpiar la consola
-    for x in range (1,base1,2):
-        y=randint(2,12)
-        if x==1:
-            # .format("\u2721") contiene la estrella
-            print(Style.BRIGHT+Fore.RED+"{:^40}".format("\u2721"))  #BRIGHT
-        elif y%5==0:
-            print(Fore.RED+"{:^40}".format(texto1[:x]))
-        elif y%3==0:
-            print(Fore.GREEN+"{:^40}".format(texto1[:x]))
+    print("\033c")
+    for letters in range (1, base, 2):
+        y = randint(2,12)
+        if letters == 1:
+            print(Style.BRIGHT + Fore.RED + "{:^40}".format("\u2721"))
+        elif y % 5 == 0:
+            print(Fore.RED + "{:^40}".format(message_to_show[:letters]))
+        elif y % 3 == 0:
+            print(Fore.GREEN + "{:^40}".format(message_to_show[:letters]))
         else:
-            print(Fore.MAGENTA+"{:^40}".format(texto1[:x]))
+            print(Fore.MAGENTA + "{:^40}".format(message_to_show[:letters]))
 
-    print(Fore.WHITE+"{:^39}".format("||||"))
-    print(Fore.WHITE+"{:^39}".format("||||"))
-    sleep(.50)
+    print(Fore.WHITE + "{:^39}".format("||||"))
+    print(Fore.WHITE + "{:^39}".format("||||"))
+    sleep(.50) 
 
